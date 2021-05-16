98e723c14b796bf67c1b854c58d3305b2f7b403a
Andrew Chung <andrewtc4@gmail.com>
2021-05-16 12:22:16 -0400

    Add add_album

--- app.py
+++ app.py
@@ -2,6 +2,48 @@ import configparser
 import mysql.connector
 import sys
 
+########################
+# Function definitions #
+########################
+
+"""
+Add an album to the database.
+
+The `comment` and `composer` fields default to None because they are not
+required in the database.
+
+Return True if the data is inserted successfully. Else, return False.
+"""
+def add_album(title, artist, genre, year, medium, type, complete,
+        comment=None, composer=None):
+    cursor = conn.cursor()
+    query = "INSERT INTO albums (title, artist, genre, year, comment,"\
+            "composer, medium, type, complete) VALUES ("\
+            f"'{title}', '{artist}', '{genre}', '{str(year)}', "
+    if comment is not None:
+        query += f"'{comment}', "
+    else:
+        query += "null, "
+    if composer is not None:
+        query += f"'{composer}', "
+    else:
+        query += "null, "
+    if medium in ['cd', 'digital', 'vinyl']:
+        query += f"'{medium}', "
+    else:
+        return False
+    if type in ["studio album", "single", "ep"]:
+        query += f"'{type}', "
+    else:
+        return False
+    if complete in ['y', 'n']:
+        query += f"'{complete}')"
+    else:
+        return False
+    cursor.execute(query)
+    conn.commit()
+    return True
+
 ############################
 # Configuring the app data #
 ############################
